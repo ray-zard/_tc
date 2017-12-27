8da6ceab6033c3ffe379582c78a57c0e726a7e91
ray-zard <ray-zard@mail.ru>
2017-12-28 03:53:54 +0400

    Допилил material_select, с тегами все еще капаюсь

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
   $('.button-collapse').sideNav();
   $('.parallax').parallax();
   $('.modal').modal();
+
   $('select').material_select();
+
   $('[data-tooltip]').tooltip({delay: 50});
   $('.slider').bxSlider();
 
