27fd389796a7305d635bea257b1aed793ea70030
ray-zard <ray-zard@mail.ru>
2017-12-29 00:59:36 +0400

    выпадающий список с тегами допилен

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,40 +1,56 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $(".button-collapse").sideNav();
   $(".parallax").parallax();
   $(".modal").modal();
 
   $("select").material_select();
 
-  $("[data-tooltip]").tooltip({ delay: 50 });
+  $("[data-tooltip]").tooltip({
+    delay: 50
+  });
   $(".slider").bxSlider();
 
-  $(".input-gropper").dropdown();
+  $(".input-dropper").dropdown();
 
-  $('#dropdown-dropper input:checkbox').change(function() {
-    if ($(this).is(':checked')) {
+  $('#dropdown-dropper :checkbox').change(function () {
 
-      console.log($(this).length);
+    var valDropper = $('#dropdown-dropper :checkbox:checked').siblings().map(function () {
+      return $(this).html()
+    });
 
-    } else {
-      $('.input-gropper span.input-gropper-valuelue').text('Рвзрешение файлов');
+    if ($(this).is(':checked') || $(this).attr('checked',false)) {
+      
+      $('.input-dropper span.input-dropper-valuelue').empty();
+      
+      $.each(valDropper, function () {
+        $('.input-dropper span.input-dropper-valuelue').append(this + ', ');
+      });
+  
+    } if (valDropper.length == 0) {
+      $('.input-dropper span.input-dropper-valuelue').text('Рвзрешение файлов');
     }
+
+    console.log($('#dropdown-dropper input:checkbox:checked').siblings().map(function () {
+      return $(this).html()
+    }).get());
+
   });
 
-  $(".input-gropper + .dropdown-content").on("click", function(e) {
+  $(".input-dropper + .dropdown-content").on("click", function (e) {
     e.stopPropagation();
   });
 
-  $(".login").on("click", "#reg", function() {
+  $(".login").on("click", "#reg", function () {
     $(".login").css("display", "none");
     $(".login.login-reg").css("display", "block");
   });
 
-  $(".val-link").click(function() {
+  $(".val-link").click(function () {
     if (
       $(this)
-        .parent()
-        .siblings()
-        .is(".val-body")
+      .parent()
+      .siblings()
+      .is(".val-body")
     ) {
       $(this).toggleClass("active");
       $(this)
@@ -46,7 +62,7 @@ $(document).ready(function() {
     }
   });
 
-  $("form.val-body :checkbox").change(function(e) {
+  $("form.val-body :checkbox").change(function (e) {
     if ($("input:checked").length > 0) {
       $(".val-f-btn").addClass("active");
     } else {
@@ -54,12 +70,12 @@ $(document).ready(function() {
     }
   });
 
-  $(".val-i-drop").click(function() {
+  $(".val-i-drop").click(function () {
     if (
       $(this)
-        .parent()
-        .siblings()
-        .is(".val-sublist")
+      .parent()
+      .siblings()
+      .is(".val-sublist")
     ) {
       $(this).toggleClass("active");
       $(this)
