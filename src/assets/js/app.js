f8a272be861b485cf67a412cdf7fdbb2b1b9d550
ray-zard <ray-zard@mail.ru>
2017-12-28 22:01:03 +0400

    ---

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,48 +1,75 @@
 $(document).ready(function() {
+  $(".button-collapse").sideNav();
+  $(".parallax").parallax();
+  $(".modal").modal();
 
-  $('.button-collapse').sideNav();
-  $('.parallax').parallax();
-  $('.modal').modal();
+  $("select").material_select();
 
-  $('select').material_select();
+  $("[data-tooltip]").tooltip({ delay: 50 });
+  $(".slider").bxSlider();
 
-  $('[data-tooltip]').tooltip({delay: 50});
-  $('.slider').bxSlider();
+  $(".input-gropper").dropdown();
 
-  $('.login').on('click', '#reg', function() {
-    $('.login').css('display', 'none');
-    $('.login.login-reg').css('display', 'block');
-  });
+  $('#dropdown-dropper input:checkbox').change(function() {
+    if ($(this).is(':checked')) {
 
-  $('.val-link').click(function() {
-    if ($(this).parent().siblings().is('.val-body')) {
-      $(this).toggleClass('active');
-      $(this).parent().siblings('.val-body').toggle();
+      console.log($(this).length);
+
     } else {
-      $(this).removeClass('active');
+      $('.input-gropper span.input-gropper-valuelue').text('Рвзрешение файлов');
     }
   });
 
-
-  $('form.val-body :checkbox').change(function(e) {
-
-    if ($('input:checked').length > 0) {
-      $('.val-f-btn').addClass('active');
-    } else {
-      $('.val-f-btn').removeClass('active');
-    }
-
+  $(".input-gropper + .dropdown-content").on("click", function(e) {
+    e.stopPropagation();
   });
 
-  $('.val-i-drop').click(function() {
-    if ($(this).parent().siblings().is('.val-sublist')) {
-      $(this).toggleClass('active');
-      $(this).parent().siblings('.val-sublist').toggle();
+  $(".login").on("click", "#reg", function() {
+    $(".login").css("display", "none");
+    $(".login.login-reg").css("display", "block");
+  });
+
+  $(".val-link").click(function() {
+    if (
+      $(this)
+        .parent()
+        .siblings()
+        .is(".val-body")
+    ) {
+      $(this).toggleClass("active");
+      $(this)
+        .parent()
+        .siblings(".val-body")
+        .toggle();
     } else {
-      $(this).removeClass('active');
+      $(this).removeClass("active");
     }
-  })
-  
+  });
+
+  $("form.val-body :checkbox").change(function(e) {
+    if ($("input:checked").length > 0) {
+      $(".val-f-btn").addClass("active");
+    } else {
+      $(".val-f-btn").removeClass("active");
+    }
+  });
+
+  $(".val-i-drop").click(function() {
+    if (
+      $(this)
+        .parent()
+        .siblings()
+        .is(".val-sublist")
+    ) {
+      $(this).toggleClass("active");
+      $(this)
+        .parent()
+        .siblings(".val-sublist")
+        .toggle();
+    } else {
+      $(this).removeClass("active");
+    }
+  });
 });
 
 // $(document).Materialize();
