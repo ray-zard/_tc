0c63b6f7dd985ff8238bdbb10a4448ab049a6c35
ray-zard <ray-zard@mail.ru>
2018-01-05 07:30:27 +0400

    Правки от Александра

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
   $(".button-collapse").sideNav();
   $(".parallax").parallax();
   $(".modal").modal();
@@ -12,57 +12,53 @@ $(document).ready(function () {
 
   $(".input-dropper").dropdown();
 
-  $('#dropdown-dropper :checkbox').change(function () {
+  $("#dropdown-dropper :checkbox").change(function() {
 
-    var valDropper = $('#dropdown-dropper :checkbox:checked').siblings().map(function () {
-      return $(this).html()
-    });
-
-    if ($(this).is(':checked') || $(this).attr('checked',false)) {
-      
-      $('.input-dropper span.input-dropper-valuelue').empty();
-      
-      $.each(valDropper, function () {
-        $('.input-dropper span.input-dropper-valuelue').append(this + ', ');
+    var valDropper = $("#dropdown-dropper :checkbox:checked")
+      .siblings()
+      .map(function() {
+        return $(this).html();
       });
-  
-    } if (valDropper.length == 0) {
-      $('.input-dropper span.input-dropper-valuelue').text('Рвзрешение файлов');
+
+    if ($(this).is(":checked") || $(this).attr("checked", false)) {
+
+      $(".input-dropper span.input-dropper-valuelue").empty();
+
+      $.each(valDropper, function() {
+        $(".input-dropper span.input-dropper-valuelue").append(this);
+      });
+    }
+    
+    if (valDropper.length == 0) {
+      $(".input-dropper span.input-dropper-valuelue").text("Рвзрешение файлов");
     }
 
-    console.log($('#dropdown-dropper input:checkbox:checked').siblings().map(function () {
-      return $(this).html()
-    }).get());
-
+    // console.log(
+    //   $("#dropdown-dropper input:checkbox:checked").siblings().map(function() {
+    //       return $(this).html();
+    //     }).get()
+    // );
   });
 
-  $(".input-dropper + .dropdown-content").on("click", function (e) {
+  $(".input-dropper + .dropdown-content").on("click", function(e) {
     e.stopPropagation();
   });
 
-  $(".login").on("click", "#reg", function () {
+  $(".login").on("click", "#reg", function() {
     $(".login").css("display", "none");
     $(".login.login-reg").css("display", "block");
   });
 
-  $(".val-link").click(function () {
-    if (
-      $(this)
-      .parent()
-      .siblings()
-      .is(".val-body")
-    ) {
+  $(".val-link").click(function() {
+    if ($(this).parent().siblings().is(".val-body")) {
       $(this).toggleClass("active");
-      $(this)
-        .parent()
-        .siblings(".val-body")
-        .toggle();
+      $(this).parent().siblings(".val-body").toggle();
     } else {
       $(this).removeClass("active");
     }
   });
 
-  $("form.val-body :checkbox").change(function (e) {
+  $("form.val-body :checkbox").change(function(e) {
     if ($("input:checked").length > 0) {
       $(".val-f-btn").addClass("active");
     } else {
@@ -70,12 +66,12 @@ $(document).ready(function () {
     }
   });
 
-  $(".val-i-drop").click(function () {
+  $(".val-i-drop").click(function() {
     if (
       $(this)
-      .parent()
-      .siblings()
-      .is(".val-sublist")
+        .parent()
+        .siblings()
+        .is(".val-sublist")
     ) {
       $(this).toggleClass("active");
       $(this)
@@ -86,6 +82,27 @@ $(document).ready(function () {
       $(this).removeClass("active");
     }
   });
+
+  // $('.ss-block .select-dropdown').bind('keyup keydown keypres select click',function(){
+  //   var l=this.value.length;
+  //   l=(l==0)? 1: l;
+  //   $('#test').attr('size',l);
+  // });
+
+  function resizeInput() {
+    $(this).attr("size", $(this).val().length);
+  }
+
+  $('.ss-select .select-dropdown, #trakerVal .select-dropdown').keyup(resizeInput).each(resizeInput);
+ 
+  $('.ss-select, #trakerVal').change(function() {
+
+    if ( $('.ss-select .select-dropdown, #trakerVal .select-dropdown').hasClass('active')) {
+      // console.log('класса нет');
+      $('.ss-select .select-dropdown, #trakerVal .select-dropdown').keyup(resizeInput).each(resizeInput);
+    }
+
+  })
 });
 
 // $(document).Materialize();
